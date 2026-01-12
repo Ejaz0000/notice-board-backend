0ac72916768d82ece287b5dbe6ed82197435f354
Ejaz Ahmed <120748404+Ejaz0000@users.noreply.github.com>
2026-01-13 00:24:13 +0600

    Fix serverless compatibility

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const connectDB = require("./src/config/db");
 
 connectDB();
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.VERCEL !== "1") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
 
 module.exports = app;
